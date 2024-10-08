5b8f2d09b0e9fdffaf7dd3ac9c0e1cbafea5fac2
Ikenna Richard <oguejioforrichard@gmail.com>
2024-10-08 22:01:57 +0100

    chore{config): update default files

--- app.vue
+++ app.vue
@@ -1,5 +1,21 @@
+<script setup>
+const isDarkMode = ref(false);
+</script>
 <template>
-  <div>
-    <h1 class="font-semibold text-2xl">Space</h1>
+  <div :class="{ dark: isDarkMode }">
+    <main class="min-h-svh dark:bg-gray-950 dark:text-gray-50">
+      <div class="grid grid-cols-12 mx-auto md:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
+        <!-- left sidebar -->
+        <div class="md:block col-span-1 xl:col-span-2">
+          <SidebarLeft />
+        </div>
+      </div>
+    </main>
   </div>
 </template>
+
+<style>
+  body {
+    font-family: "Inter", sans-serif;
+  }
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/fonts"],
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+});

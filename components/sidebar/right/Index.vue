1630a271a0b24788c8bc92c0dfce24092b37e528
Ikenna Richard <oguejioforrichard@gmail.com>
2024-10-12 19:54:44 +0100

    feat: add right sidebar

---
+++ components/sidebar/right/Index.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+  <aside class="flex flex-col">
+    <SidebarRightPreviewCard title="What's happening">
+      <SidebarRightPreviewCardItem />
+    </SidebarRightPreviewCard>
+
+    <SidebarRightPreviewCard title="Who to follow">
+      <SidebarRightPreviewCardItem />
+    </SidebarRightPreviewCard>
+  </aside>
+</template>
---
+++ components/sidebar/right/PreviewCard/Index.vue
@@ -0,0 +1,22 @@
+<script setup>
+const { spaceBorderColor } = useTransition();
+const props = defineProps({
+  title: String,
+  required: true,
+});
+</script>
+
+<template>
+  <div
+    class="m-2 border border-gray-50 overflow-hidden"
+    :class="spaceBorderColor"
+  >
+    <h1 class="p-3 text-xl font-extrabold text-gray-900 border-b dark:text-gray-50">
+      {{ props.title }}
+    </h1>
+    <slot></slot>
+    <div class="p-3 text-sm text-gray-800 cursor-pointer hover:bg-gray-100 dark:text-gray-50 dark:hover:bg-gray-900">
+      Show more
+    </div>
+  </div>
+</template>
